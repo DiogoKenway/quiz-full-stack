4a3999291a803f7d600f9eead9ee9fca1d47ab6d
Diogo Alves <diogodidi2022@gmail.com>
2023-02-04 23:58:24 -0300

    adding more code

--- model/questao.ts
+++ model/questao.ts
@@ -1,6 +1,11 @@
 export default class QuestaoModel {
     #id: number;
     #enuncido: string;
-    
+    #resposta: any[];
+    #acertou: boolean;
+    // #respondida: boolean;
 
+    constructor(id: number, enunciado: string, resposta: ) {
+
+    }
 }
