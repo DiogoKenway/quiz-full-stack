3042f36ba048933253db78344410ed1ca922620e
Diogo Alves <diogodidi2022@gmail.com>
2023-02-05 00:34:26 -0300

    implementing the project model class

--- model/questao.ts
+++ model/questao.ts
@@ -1,18 +1,35 @@
 export default class QuestaoModel {
     #id: number;
-    #enuncido: string;
+    #enunciado: string;
     #resposta: any[];
     #acertou: boolean;
     // #respondida: boolean;
 
     constructor(id: number, enunciado: string, resposta: any[], acertou: boolean) {
         this.#id = id;
-        this.#enuncido = enunciado;
+        this.#enunciado = enunciado;
         this.#resposta = resposta;
         this.#acertou = acertou;
     }
 
     get id() {
         return this.#id;
+    }
+
+    get enunciado() {
+        return this.#enunciado;
+    }
+
+    get resposta() {
+        return this.#resposta;
+    }
+
+    get acertou() {
+        return this.#acertou;
+    }
+
+    get respondida() {
+        // FIXME: implementar esse método
+        return false;
     }
 }
