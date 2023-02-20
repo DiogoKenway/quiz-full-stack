39446b14c751fe5f59340b8b6d2f9f45bbbb197e
Diogo Alves <diogodidi2022@gmail.com>
2023-02-19 22:22:55 -0300

    adding filter

--- pages/api/questoes/[id].ts
+++ pages/api/questoes/[id].ts
@@ -3,7 +3,11 @@ import questoes from "../bancoDeQuestoes";
 export default  (req: any, res: any) => {
     const idSelecionado = +req.query.id;
 
-    
+    const unicaQuestaoOuNada = questoes.filter(questao => questao.id === idSelecionado);
+
+    if(unicaQuestaoOuNada.length === 1) {
+      
+    }
 
     res.status(200).json(questoes[0].paraObjeto());
   }
