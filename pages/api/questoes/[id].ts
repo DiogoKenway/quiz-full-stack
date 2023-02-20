0c3e6125b57aef284e67afd26dac2cf83ffc4abc
Diogo Alves <diogodidi2022@gmail.com>
2023-02-19 22:15:50 -0300

    adding code

--- pages/api/questoes/[id].ts
+++ pages/api/questoes/[id].ts
@@ -1,6 +1,9 @@
 import questoes from "../bancoDeQuestoes";
 
 export default  (req: any, res: any) => {
+    const idSelecionado = +req.query.id;
+
+    
 
     res.status(200).json(questoes[0].paraObjeto());
   }
