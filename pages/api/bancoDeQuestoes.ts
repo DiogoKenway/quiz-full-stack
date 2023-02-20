46dbdffe03b680557c105daa9647ed4aa0bf3881
Diogo Alves <diogodidi2022@gmail.com>
2023-02-20 00:14:24 -0300

    changing id

--- pages/api/bancoDeQuestoes.ts
+++ pages/api/bancoDeQuestoes.ts
@@ -2,7 +2,7 @@ import QuestaoModel from "../../model/questao";
 import RespostaModel from "../../model/resposta";
 
 const questoes : QuestaoModel[] = [
-    new QuestaoModel(306, "Qual bicho transmite a doença de chagas?", [
+    new QuestaoModel(201, "Qual bicho transmite a doença de chagas?", [
         RespostaModel.errada("Abelha"),
         RespostaModel.errada("Barata"),
         RespostaModel.errada("Pulga"),
